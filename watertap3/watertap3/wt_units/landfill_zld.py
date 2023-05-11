eaa49e6c5abc55a7b9a711770d088421a3c7c547
Mukta Hardikar <Mukta.Hardikar@nrel.gov>
2023-05-11 14:15:35 -0600

    Transport added to cost

--- watertap3/watertap3/wt_units/landfill_zld.py
+++ watertap3/watertap3/wt_units/landfill_zld.py
@@ -16,7 +16,7 @@ tpec_or_tic = 'TPEC'
 
 class UnitProcess(WT3UnitProcess):
 
-    def fixed_cap(self):
+    def fixed_cap(self,unit_params):
         time = self.flowsheet().config.time.first()
         self.flow_in = pyunits.convert(self.flow_vol_in[time], to_units=pyunits.m ** 3 / pyunits.hr)
         self.capacity_basis = 302096
@@ -28,7 +28,11 @@ class UnitProcess(WT3UnitProcess):
         self.total_mass = total_mass = self.density * self.flow_in
         self.chem_dict = {}
         landfill_cap = (self.total_mass / self.capacity_basis) ** self.cap_scaling_exp
-        return landfill_cap
+        transport_fixed_capital = 3.42
+        if unit_params['distance'] == 0:
+            return landfill_cap
+        else:
+            return landfill_cap + transport_fixed_capital
 
     def elect(self):
         electricity = 0
@@ -39,8 +43,10 @@ class UnitProcess(WT3UnitProcess):
         Initialize the unit in WaterTAP3.
         '''
         financials.create_costing_block(self, basis_year, tpec_or_tic)
-        self.costing.fixed_cap_inv_unadjusted = Expression(expr=self.fixed_cap(),
+        self.costing.fixed_cap_inv_unadjusted = Expression(expr=self.fixed_cap(unit_params),
                                                            doc='Unadjusted fixed capital investment')
+        
         self.electricity = Expression(expr=self.elect(),
                                       doc='Electricity intensity [kwh/m3]')
+        
         financials.get_complete_costing(self.costing)
